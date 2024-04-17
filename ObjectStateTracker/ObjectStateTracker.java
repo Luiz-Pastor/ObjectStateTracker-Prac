a230b8653ef8ab4b9198af827e3f6a2d0b78051f
Luiz-Pastor <luigpacr5@gmail.com>
2024-04-18 00:20:26 +0200

    Completed the method to add and update object. The second one might not be working fine. The topic of the exception is also missing

--- ObjectStateTracker/ObjectStateTracker.java
+++ ObjectStateTracker/ObjectStateTracker.java
@@ -6,7 +6,7 @@ import java.util.function.Predicate;
 /*                       <Object , State> */
 public class ObjectStateTracker<O, S> {
     
-    //List<S> states = new ArrayList<>();
+    private Map<O, S> objects = new LinkedHashMap<>();
     private Map<S, Predicate<O>> states = new TreeMap<>();
     private S defaultState;
     
@@ -28,6 +28,8 @@ public class ObjectStateTracker<O, S> {
     
     /*____________________________________________________________________*/
     
+
+    
     public ObjectStateTracker<O,S> withState(S state, Predicate<O> function) {
         this.states.put(state, function);
         return this;
@@ -37,22 +39,59 @@ public class ObjectStateTracker<O, S> {
         this.defaultState = state;
         return this;
     }
+
+    private S getCurrentState(O object) {
+        for (S state : this.states.keySet()) {
+            if (this.states.get(state).test(object) == true)
+                return state;
+        }
+        return this.defaultState;
+    }
+    
+    public void addObjects(O ...objects) {
+        for (O currentObject : objects) {
+            this.objects.put(currentObject, this.getCurrentState(currentObject));
+        }
+    }
+    
+    public void updateStates() {
+        for (O currentObject : this.objects.keySet()) {
+            this.objects.put(currentObject, this.getCurrentState(currentObject));
+        }
+    }
     
     @Override
     public String toString() {
-        int index;
+        int stateCount = this.states.size();
         String buffer;
+        boolean flag;
         
-        buffer = "{ ";
-        index = 0;
-        for (S current : this.states.keySet()) {
-            buffer += current;
-            index++;
+        buffer = "{";       
+        /* Print the info of each state */
+        for (S currentState : this.states.keySet()) {
+            flag = false;
+            buffer += currentState + "=[";
             
-            if (index != this.states.size())
+            for (O currentObject : this.objects.keySet()) {
+                if (this.objects.get(currentObject) == currentState) {
+                    /* If there has been an element before, a comma is put */
+                    if (flag == false)
+                        flag = true;
+                    else
+                        buffer += ", ";
+                    
+                    /* The object is added */
+                    buffer += currentObject;
+                }
+            }
+            buffer += "]";
+            
+            /* If there is another element after, a comma is added */
+            stateCount--;
+            if (stateCount > 0)
                 buffer += ", ";
         }
-        buffer += " }";
+        buffer += "}";
         return buffer;
     }
     
