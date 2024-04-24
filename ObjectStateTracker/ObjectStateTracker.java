e9f9c46f0251284a20238353f3ef8b8c870e7277
Luiz-Pastor <luigpacr5@gmail.com>
2024-04-24 23:42:57 +0200

    Implemented the Trajectory class, to get the information easily (over all on the part where the trajectory information is important to be checker, not just on a sting msg)

--- ObjectStateTracker/ObjectStateTracker.java
+++ ObjectStateTracker/ObjectStateTracker.java
@@ -1,6 +1,5 @@
 package ObjectStateTracker;
 
-import java.time.LocalDateTime;
 import java.util.*;
 import java.util.function.Predicate;
 
@@ -8,7 +7,7 @@ import java.util.function.Predicate;
 public class ObjectStateTracker<O, S> {
 
     /* Object list, with the object and the list of states with his time */
-    private final Map<O, LinkedHashMap<S, LocalDateTime>> objects = new LinkedHashMap<>();
+    private final Map<O, Trajectory<S>> objects = new LinkedHashMap<>();
 
     /* State set, state-lambda map and default state*/
     private final Set<S> states = new TreeSet<>();
@@ -48,38 +47,31 @@ public class ObjectStateTracker<O, S> {
     }
 
     public void addObjects(O... objects) {
+        
+        /* Adding an entry on the list for each new object, setting an initial state */
         for (O currentObject : objects) {
-            /* Create a linked hash map, to store the init state and the time */
-            LinkedHashMap<S, LocalDateTime> tempMap = new LinkedHashMap<>();
-            tempMap.put(this.getCurrentState(currentObject), LocalDateTime.now());
-
-            /* Set the value of the current object */
-            this.objects.put(currentObject, tempMap);
+            this.objects.put(currentObject, new Trajectory<>(this.getCurrentState(currentObject)));
         }
     }
 
     /*____________________________________________________________________*/
     public void updateStates() {
         for (O currentObject : this.objects.keySet()) {
-
             /* Get the new state */
             S newState = this.getCurrentState(currentObject);
-
+            
             /* Get the previous list of states */
-            LinkedHashMap<S, LocalDateTime> tempMap = this.objects.get(currentObject);
-
+            Trajectory<S> objectTrajectory = this.objects.get(currentObject);
+            
             /* Check the last state, if it is the same than the new state, continue with the next element */
-            S last = null;
-            for (Map.Entry<S, LocalDateTime> it : tempMap.entrySet()) {
-                last = it.getKey();
-            }
+            S last = objectTrajectory.last();
             if (last == null || last == newState) {
                 continue;
             }
 
             /* If it is a new state, add the state to the object trajectory */
-            tempMap.put(newState, LocalDateTime.now());
-            this.objects.put(currentObject, tempMap);
+            objectTrajectory.add(newState);
+            this.objects.put(currentObject, objectTrajectory); /* TODO: se puede quitar? modificando la referencia en si */
         }
     }
 
@@ -91,23 +83,18 @@ public class ObjectStateTracker<O, S> {
 
         buffer = "{";
         /* Print the info of each state */
-        for (S currentState : this.states) {
+        for (S savingState : this.states) {
             flag = false;
-            buffer += currentState + "=[";
+            buffer += savingState + "=[";
 
             /* Iterate the users list */
-            for (Map.Entry<O, LinkedHashMap<S, LocalDateTime>> currentEntry : this.objects.entrySet()) {
+            for (Map.Entry<O, Trajectory<S>> currentEntry : this.objects.entrySet()) {
                 O currentObject = currentEntry.getKey();
-                LinkedHashMap<S, LocalDateTime> currentTrajectory = currentEntry.getValue();
+                Trajectory<S> currentTrajectory = currentEntry.getValue();
 
-                /* Get his last state */
-                S lastObjectState = null;
-                for (S objectState : currentTrajectory.keySet()) {
-                    lastObjectState = objectState;
-                }
-
-                /* If it is the same state, it is saved */
-                if (lastObjectState == currentState) {
+                /* If the last state it is the same that the one that is being printing, it is saved */
+                S lastState = currentTrajectory.last();
+                if (lastState == savingState) {
                     /* If there has been an element before, a comma is put */
                     if (flag == false) {
                         flag = true;
@@ -131,45 +118,18 @@ public class ObjectStateTracker<O, S> {
         return buffer;
     }
 
-    public String trajectory(O o) {
-        Map.Entry<O, LinkedHashMap<S, LocalDateTime>> searchEntry = null;
-        for (Map.Entry<O, LinkedHashMap<S, LocalDateTime>> currentEntry : this.objects.entrySet()) {
+    public Trajectory<S> trajectory(O o) {
+        Map.Entry<O, Trajectory<S>> searchEntry = null;
+        
+        /* Search the entry of the specified object */
+        for (Map.Entry<O, Trajectory<S>> currentEntry : this.objects.entrySet()) {
             if (currentEntry.getKey().equals(o)) {
                 searchEntry = currentEntry;
                 break;
             }
         }
 
-        if (searchEntry == null) {
-            return null;
-        }
-
-        String buffer = "[";
-        S beforeState = null;
-        int index = 0, max = searchEntry.getValue().entrySet().size();
-        for (Map.Entry<S, LocalDateTime> currentData : searchEntry.getValue().entrySet()) {
-            /* Get the entry data */
-            S currentState = currentData.getKey();
-            LocalDateTime time = currentData.getValue();
-
-            /* Check what msg to print */
-            if (beforeState == null) {
-                buffer += "(in: " + currentState + " at: " + time + ")";
-            } else {
-                buffer += "(from: " + beforeState + " to " + currentState + " at: " + time + ")";
-            }
-
-            /* Check if there are going to be more msgs */
-            index++;
-            if (index < max) {
-                buffer += ", ";
-            }
-
-            /* Update the last state */
-            beforeState = currentState;
-        }
-
-        buffer += "]";
-        return buffer;
+        /* Returning the trajectory of the object, or null if it is not saved */
+        return (searchEntry == null) ? null : searchEntry.getValue();
     }
 }
