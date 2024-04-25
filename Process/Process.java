8ce33f5969a132ca7c7cb1b48771320b55e5e92e
Luiz-Pastor <luigpacr5@gmail.com>
2024-04-25 12:37:40 +0200

    Created a list to save the count of connections od each state

--- Process/Process.java
+++ Process/Process.java
@@ -1,23 +1,60 @@
 package Process;
 
 import java.util.*;
+import ObjectStateTracker.*;
 
 public class Process<S> {
     
-    private Map<S, List<Integer>> states = new TreeMap<>();
+    private final Set<S> states = new TreeSet<>();
+    private final Map<S, Integer> statesBegin = new HashMap<>();
+    private final Map<S, Integer> statesFinal = new HashMap<>();
+    private final Map<S, Map<S, Integer>> statesConnection = new HashMap<>();
     
+    /*
+     * -> Save states. For each states
+     *      · Save the times that it is the initial state
+     *      · Save the times that it is the final state
+     *      · Which state is going to go, and how many times
+    */
     public Process(S ...values) {
+        /* Add the states to the set */
+        this.states.addAll(Arrays.asList(values));
+        
+        /* Init all the states value */
         for (S currentState : values) {
-            List<Integer> dataList = new ArrayList<>();
-            dataList.add(0);
-            dataList.add(0);
-            this.states.put(currentState, dataList);
+            this.statesBegin.put(currentState, 0);
+            this.statesFinal.put(currentState, 0);
+        }
+        
+        for (S stateToSave : values) {
+            Map<S, Integer> connections = new HashMap<>();
+            
+            for (S stateToAdd : values) {
+                if (stateToSave != stateToAdd)
+                    connections.put(stateToAdd, 0);
+            }
+            
+            this.statesConnection.put(stateToSave, connections);
         }
     }
     
     
-    public void add(String trayectory) {
+    public void add(Trajectory<S> trayectory) {
         
     }
     
+    @Override
+    public String toString() {
+        String buffer = "";
+        
+        for (S printedState : this.states) {
+            buffer += printedState + "():\n";
+            
+            
+            for (Map.Entry<S, Integer> connections : this.statesConnection.get(printedState).entrySet()) {
+                buffer += "\t# " + connections + " #\n";
+            }
+        }
+        return buffer;
+    }
 }
