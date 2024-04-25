b3714a86f590636b0f8212d78625bdab273c8d44
Luiz-Pastor <luigpacr5@gmail.com>
2024-04-25 10:43:20 +0200

    Added the tester that was deleted by an error of NetBeans

---
+++ Tests/TesterProcess.java
@@ -0,0 +1,35 @@
+package Tests;
+
+import Process.Process;
+import Registration.*;
+import static Registration.RegistrationState.*;
+import static Registration.RegistrationKind.*;
+import ObjectStateTracker.*;
+
+public class TesterProcess extends TesterTrajectories {
+
+    public static void main(String[] args) {
+        TesterProcess tsc = new TesterProcess();
+        tsc.createRegistrations();
+        tsc.changeRegistrations();
+        tsc.displayTrajectories();
+        tsc.buildProcess();
+    }
+
+    @Override
+    protected void changeRegistrations() {
+        super.changeRegistrations();
+        this.lisaMartin.setValidated(true);
+        this.regState.updateStates();
+        this.lisaMartin.pay(MEMBER.getPrice());
+        this.regState.updateStates();
+    }
+
+    protected void buildProcess() {
+        Process<RegistrationState> regProcess = new Process<>(RegistrationState.values());
+        for (Registration r : this.regState) // iterates on all Registrations
+            System.out.println("=> " + r);
+            //regProcess.add(this.regState.trajectory(r));
+        System.out.println(regProcess);
+    }
+}
