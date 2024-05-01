ff5ef8a5f1f8f128cad31ce253b28b3422245516
Luiz-Pastor <luigpacr5@gmail.com>
2024-05-01 14:15:31 +0200

    Added the last test

---
+++ Tests/TesterObserver.java
@@ -0,0 +1,26 @@
+package Tests;
+
+import Registration.*;
+import static Registration.RegistrationState.*;
+import static Registration.RegistrationKind.*;
+import ObjectStateTracker.*;
+
+public class TesterObserver extends TesterStateChanges {
+
+    public static void main(String[] args) {
+        TesterObserver tsc = new TesterObserver();
+        tsc.createRegistrations();
+        Registration.withTracker(tsc.regState);
+        System.out.println(tsc.regState);
+        tsc.changeRegistrations();
+        System.out.println(tsc.regState);
+    }
+
+    @Override
+    protected void changeRegistrations() {
+        this.annSmith.setAffiliation("University of Miskatonic"); // now it is filled
+        this.johnDoe.pay(STUDENT.getPrice());
+        // becomes payed
+        // regState.updateStates(); // not needed anymore
+    }
+}
